cb5cccce4413cc197ba5902a1423ae487449b307
Maxim-Markov <42976416+Maxim-Markov@users.noreply.github.com>
2020-12-09 12:03:23 +0300

    Add files via upload

---
+++ CourseProject.Test/KeyCheckerTest.cs
@@ -0,0 +1,27 @@
+﻿using CourseProject.Other_classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject.Test
+{
+    [TestClass]
+    public class KeyCheckerTest
+    {
+
+        [TestMethod]
+        public void CheckKey_EmptyKey_Returnnull()
+        {
+            string[] decryptedData = null;
+            string key = "";
+
+            TextEncoder encoder = new TextEncoder();
+            string[] actual = encoder.Encrypt(decryptedData, key);
+
+            Assert.IsNull(actual);
+        }
+    }
+}
